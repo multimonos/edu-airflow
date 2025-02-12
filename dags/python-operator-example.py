b8cd8888df7f9f8659a29eb91c333c88c571db1c
Craig Hopgood <searaig@gmail.com>
2025-02-12 10:42:40 -0500

    py operator hello world

---
+++ dags/python-operator-example.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.python import PythonOperator
+
+
+def ohai():
+    print("hello world")
+
+
+default_args = {
+    "owner": "multimonos",
+    "retries": 5,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    default_args=default_args,
+    dag_id="python_op_example",
+    description="hello world with python operator",
+    start_date=datetime(2025, 2, 11),
+    schedule_interval="@daily",
+) as dag:
+    task1 = PythonOperator(task_id="ohai", python_callable=ohai)
