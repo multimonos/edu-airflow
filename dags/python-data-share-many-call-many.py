75360a70c8fb203d6a1c0150f4c8d5e54770686e
Craig Hopgood <searaig@gmail.com>
2025-02-12 12:09:38 -0500

    consume + produce many to xcom

--- dags/python-data-share-many-call-many.py
+++ dags/python-data-share-many-call-many.py
@@ -4,17 +4,23 @@ from airflow.operators.python import PythonOperator
 from airflow.models.taskinstance import TaskInstance
 
 
-def greet(age: int, ti: TaskInstance) -> None:
+def greet(ti: TaskInstance) -> None:
     """pull the values we need from xcom"""
     first_name: str = ti.xcom_pull(task_ids="get_name", key="first_name")
     last_name: str = ti.xcom_pull(task_ids="get_name", key="last_name")
+    age: int = ti.xcom_pull(task_ids="get_age", key="age")
     print(f"hello {first_name} {last_name} ... you are {age} years old")
 
 
 def get_name(ti: TaskInstance) -> None:
-    """push many values to xcom for retrieval by greet fn"""
-    ti.xcom_push(key="first_name", value="Sam")
-    ti.xcom_push(key="last_name", value="Bammmz")
+    """set firstname, lastname via xcom"""
+    ti.xcom_push(key="first_name", value="Blind")
+    ti.xcom_push(key="last_name", value="Arsmemmy")
+
+
+def get_age(ti: TaskInstance) -> None:
+    """set age in xcom"""
+    ti.xcom_push(key="age", value=48)
 
 
 default_args = {
@@ -36,4 +42,6 @@ with DAG(
 
     task2 = PythonOperator(task_id="get_name", python_callable=get_name)
 
-    task2 >> task1  # pyright: ignore[reportUnusedExpression]
+    task3 = PythonOperator(task_id="get_age", python_callable=get_age)
+
+    [task2, task3] >> task1  # pyright: ignore[reportUnusedExpression]
