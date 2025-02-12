bfc23d40ef39b3145b312113fefc0a6d6b63241f
Craig Hopgood <searaig@gmail.com>
2025-02-12 10:46:50 -0500

    simple py operator fn with param

---
+++ dags/python-params.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.python import PythonOperator
+
+
+def greet(name):
+    print(f"hello {name}")
+
+
+default_args = {
+    "owner": "multimonos",
+    "retries": 5,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    default_args=default_args,
+    dag_id="python_op_parameterized",
+    description="python operator fn with params",
+    start_date=datetime(2025, 2, 11),
+    schedule_interval="@daily",
+) as dag:
+    task1 = PythonOperator(
+        task_id="greet", python_callable=greet, op_kwargs={"name": "searaig"}
+    )
