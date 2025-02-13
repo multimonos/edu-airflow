069598ac9c6613d90671f70f3fb457fdfbd655d4
Craig Hopgood <searaig@gmail.com>
2025-02-13 12:36:51 -0500

    test mysql conn by reading a few items

---
+++ dags/mysql_conn.py
@@ -0,0 +1,31 @@
+from airflow.operators.python import PythonOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
+from datetime import datetime, timedelta
+from airflow import DAG
+
+
+def fetch_data():
+    mysql_hook = MySqlHook(mysql_conn_id="devmysql")
+    conn = mysql_hook.get_conn()
+    cursor = conn.cursor()
+    cursor.execute("select * from test limit 10;")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
+
+
+default_args = {
+    "owner": "multimonos",
+    "retries": 5,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id="mysql_hook_example",
+    default_args=default_args,
+    start_date=datetime(2025, 2, 12),
+    schedule_interval="@daily",
+) as dag:
+    task1 = PythonOperator(task_id="fetch_from_mysql", python_callable=fetch_data)
